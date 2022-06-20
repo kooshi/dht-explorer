dfddbab7720049e97adf58f4ed1b921ce09dddd0
kooshi <1934337+kooshi@users.noreply.github.com>
2022-06-19 21:11:20 -0500

    refactor kmsg to more files

---
+++ src/krpc/kmsg/response.rs
@@ -0,0 +1,53 @@
+use super::*;
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq, Eq)]
+pub struct Response {
+    // ID of the querying node
+    pub id:String,
+    
+    // K closest nodes to the requested target
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    //#[serde(flatten)]
+    pub nodes: Option<CompactIPv4NodeInfo>,
+
+    // Token for future announce_peer
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    pub token:Option<String>,
+
+    // Torrent peers
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    pub values:Option<Vec<SocketAddrWrapper>>,
+
+    #[serde(flatten)]
+    pub bep44:ResponseBep44
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq, Eq)]
+pub struct ResponseBep44 {
+    // Data stored in a put message (encoded size < 1000)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    pub v:Option<String>,
+
+    // Seq of a mutable msg
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    pub seq:Option<i64>,
+
+    // ed25519 public key (32 bytes string) of a mutable msg
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    #[serde(with = "serde_bytes")]
+    pub k:Option<Vec<u8>>,
+
+    // ed25519 signature (64 bytes string)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(default)]
+    #[serde(with = "serde_bytes")]
+    #[serde(rename = "sig")]
+    pub sign:Option<Vec<u8>>,
+}
